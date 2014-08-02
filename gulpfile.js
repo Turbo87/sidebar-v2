c88a11da23cf46e96dfbfc2641a65e27d9864290
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-08-02 17:31:18 +0200

    Added gulp dev environment

---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var csslint = require('gulp-csslint');
+var header = require('gulp-header');
+var jshint = require('gulp-jshint');
+var minifyCSS = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var zip = require('gulp-zip');
+
+var pkg = require('./package.json');
+var basename = pkg.name + '-' + pkg.version;
+var banner = '/*! ${pkg.name} v${pkg.version} */\n\n';
+
+// SASS compilation
+gulp.task('sass', function () {
+    gulp.src('src/*.scss')
+        .pipe(sass({
+          includePaths: require('node-bourbon').includePaths
+        }))
+        .pipe(gulp.dest('src'));
+});
+
+// Lint JS + CSS
+gulp.task('lint', ['lint:js', 'lint:css']);
+
+gulp.task('lint:js', function() {
+  return gulp.src('src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter());
+});
+
+gulp.task('lint:css', ['sass'], function() {
+  return gulp.src('src/*.css')
+    .pipe(csslint({
+      'adjoining-classes': false,
+      'box-sizing': false,
+      'fallback-colors': false
+    }))
+    .pipe(csslint.reporter());
+});
+
+// Concat JS + CSS
+gulp.task('concat', ['concat:js', 'concat:css']);
+
+gulp.task('concat:js', function() {
+  return gulp.src('src/*.js')
+    .pipe(concat(basename +'.js'))
+    .pipe(header(banner, { pkg : pkg } ))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('concat:css', ['sass'], function() {
+  return gulp.src('src/*.css')
+    .pipe(concat(basename +'.css'))
+    .pipe(header(banner, { pkg : pkg } ))
+    .pipe(gulp.dest('dist'));
+});
+
+// Minify JS + CSS
+gulp.task('minify', ['minify:js', 'minify:css']);
+
+gulp.task('minify:js', ['concat:js'], function() {
+  return gulp.src('dist/' + basename + '.js')
+    .pipe(rename(basename + '.min.js'))
+    .pipe(uglify({
+      preserveComments: 'some'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify:css', ['concat:css'], function() {
+  return gulp.src('dist/' + basename + '.css')
+    .pipe(rename(basename + '.min.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist'));
+});
+
+// Package for distribution
+gulp.task('zip', ['minify'], function() {
+  return gulp.src([
+    'README.md',
+    'LICENSE',
+    'dist/' + basename + '.js',
+    'dist/' + basename + '.min.js',
+    'dist/' + basename + '.css',
+    'dist/' + basename + '.min.css',
+  ])
+  .pipe(rename(function (path) {
+    path.dirname = '';
+  }))
+  .pipe(zip(basename + '.zip'))
+  .pipe(gulp.dest('dist'));
+});
+
+// Cleanup
+gulp.task('clean', function() {
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+// Watch JS + CSS Files
+gulp.task('watch', ['lint'], function(){
+  gulp.watch('src/*.js', ['lint:js']);
+  gulp.watch('src/*.scss', ['lint:css']);
+});
+
+// Default
+gulp.task('default', ['lint', 'minify']);
