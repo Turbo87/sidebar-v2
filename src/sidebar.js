aea730103e6c5027bc8935b5632e16dddbeacd6d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-08-03 20:17:46 +0200

    js: First draft with correct behavior

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,9 +1,33 @@
 $('.sidebar-tabs > li > a').on('click', function(e) {
-    var sidebar = $('.sidebar');
-    if (sidebar.hasClass('collapsed'))
-        sidebar.removeClass('collapsed');
-    else
-        sidebar.addClass('collapsed');
+    var $tab = $(this).closest('li');
+    var $sidebar = $tab.closest('.sidebar');
+    var $tabs = $sidebar.find('.sidebar-tabs');
+    var $container = $sidebar.find('.sidebar-content');
+
+    if ($tab.hasClass('active')) {
+        // remove old active highlights
+        $tabs.children('li.active').removeClass('active');
+
+        // close sidebar
+        $sidebar.addClass('collapsed');
+
+    } else {
+        // hide old active contents
+        $container.children('.sidebar-pane.active').removeClass('active');
+
+        // show new content
+        $container.children(this.hash).addClass('active');
+
+        // remove old active highlights
+        $tabs.children('li.active').removeClass('active');
+
+        // set new highlight
+        $tab.addClass('active');
+
+        if ($sidebar.hasClass('collapsed'))
+            // open sidebar
+            $sidebar.removeClass('collapsed');
+    }
 });
 
 $(function () {
