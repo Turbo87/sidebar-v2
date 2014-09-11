03a6bc72401157f8da2f2b0ab0d89082b1f23380
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-09-12 01:34:47 +0200

    js/jquery: Extend the jQuery fn namespace

--- js/jquery-sidebar.js
+++ js/jquery-sidebar.js
@@ -1,31 +1,35 @@
-$('.sidebar-tabs > li > a').on('click', function(e) {
-    var $tab = $(this).closest('li');
-    var $sidebar = $tab.closest('.sidebar');
-    var $tabs = $sidebar.find('.sidebar-tabs');
-    var $container = $sidebar.find('.sidebar-content');
+$.fn.sidebar = function() {
+    this.find('.sidebar-tabs > li > a').on('click', function() {
+        var $tab = $(this).closest('li');
+        var $sidebar = $tab.closest('.sidebar');
+        var $tabs = $sidebar.find('.sidebar-tabs');
+        var $container = $sidebar.find('.sidebar-content');
 
-    if ($tab.hasClass('active')) {
-        // remove old active highlights
-        $tabs.children('li.active').removeClass('active');
+        if ($tab.hasClass('active')) {
+            // remove old active highlights
+            $tabs.children('li.active').removeClass('active');
 
-        // close sidebar
-        $sidebar.addClass('collapsed');
+            // close sidebar
+            $sidebar.addClass('collapsed');
 
-    } else {
-        // hide old active contents
-        $container.children('.sidebar-pane.active').removeClass('active');
+        } else {
+            // hide old active contents
+            $container.children('.sidebar-pane.active').removeClass('active');
 
-        // show new content
-        $container.children(this.hash).addClass('active');
+            // show new content
+            $container.children(this.hash).addClass('active');
 
-        // remove old active highlights
-        $tabs.children('li.active').removeClass('active');
+            // remove old active highlights
+            $tabs.children('li.active').removeClass('active');
 
-        // set new highlight
-        $tab.addClass('active');
+            // set new highlight
+            $tab.addClass('active');
 
-        if ($sidebar.hasClass('collapsed'))
-            // open sidebar
-            $sidebar.removeClass('collapsed');
-    }
-});
+            if ($sidebar.hasClass('collapsed'))
+                // open sidebar
+                $sidebar.removeClass('collapsed');
+        }
+    });
+
+    return this;
+};
