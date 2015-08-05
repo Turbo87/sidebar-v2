ef01fa567987ecb0083be1c872df5f9b151be219
Matthew Mueller <MuellerMatthew@users.noreply.github.com>
2015-08-05 11:20:56 -0400

    adding additional handles to sidebar
    Added several public and private functions to sidebar to give users more control.
    
    New public functions:
     ``getActiveTab()`` Returns the currently shown sidebar tab.
    ``getDisabledTabs()`` Returns a list of the currently disabled tabs.
    New private functions:
     ``_getActiveTab()`` Returns the currently shown sidebar tab(copied existing code which was in close function to create this function).
    ``_getDisabledTabs()`` Returns a list of the currently disabled tabs.
    
    Modified the ``open`` function so it:
    1. checks whether a tab is disabled before opening it, if the tab is disabled, it won't open.
    2. checks if the id it is trying to open actually exists before opening the tab.  If the ID doesn't match it won't open anything. 
    
    modified the ``close`` function so it uses the new ``_this.getActiveTab()`` code to simplify the function.

--- js/leaflet-sidebar.js
+++ js/leaflet-sidebar.js
@@ -89,44 +89,52 @@ L.Control.Sidebar = L.Control.extend({
     },
 
     open: function(id) {
-        var i, child;
+        var i, child
+            disabled = this._getDisabledTabs(); //getting list of disabled tabs
+            
+        for(i = disabled.length-1; i>=0; i--) {
+            if(disabled[i].firstChild.hash =='#' + id)
+                return this;                        //if the tab is disabled, then do not try to open it.  
+        }
 
         // hide old active contents and show new content
         for (i = this._panes.length - 1; i >= 0; i--) {
             child = this._panes[i];
-            if (child.id == id)
-                L.DomUtil.addClass(child, 'active');
-            else if (L.DomUtil.hasClass(child, 'active'))
-                L.DomUtil.removeClass(child, 'active');
+            if (child.id == id) {   //the ID must match one of the existing tabs to continue, else, return
+        
+                for (i = this._panes.length - 1; i >= 0; i--) {  
+                    if (child.id == id)
+                        L.DomUtil.addClass(child, 'active');
+                    else if (L.DomUtil.hasClass(child, 'active'))
+                        L.DomUtil.removeClass(child, 'active');
+                }
+
+                // remove old active highlights and set new highlight
+                for (i = this._tabitems.length - 1; i >= 0; i--) {
+                    child = this._tabitems[i];
+                    if (child.querySelector('a').hash == '#' + id)
+                        L.DomUtil.addClass(child, 'active');
+                    else if (L.DomUtil.hasClass(child, 'active'))
+                        L.DomUtil.removeClass(child, 'active');
+                }
+
+                this.fire('content', { id: id });
+        
+                // open sidebar (if necessary)
+                if (L.DomUtil.hasClass(this._sidebar, 'collapsed')) {
+                    this.fire('opening');
+                    L.DomUtil.removeClass(this._sidebar, 'collapsed');
+                }
+                return this;
+            }
         }
-
-        // remove old active highlights and set new highlight
-        for (i = this._tabitems.length - 1; i >= 0; i--) {
-            child = this._tabitems[i];
-            if (child.querySelector('a').hash == '#' + id)
-                L.DomUtil.addClass(child, 'active');
-            else if (L.DomUtil.hasClass(child, 'active'))
-                L.DomUtil.removeClass(child, 'active');
-        }
-
-        this.fire('content', { id: id });
-
-        // open sidebar (if necessary)
-        if (L.DomUtil.hasClass(this._sidebar, 'collapsed')) {
-            this.fire('opening');
-            L.DomUtil.removeClass(this._sidebar, 'collapsed');
-        }
-
-        return this;
+        return this; //if the ID doesn't match any of the tabs, then just return
     },
 
     close: function() {
         // remove old active highlights
-        for (var i = this._tabitems.length - 1; i >= 0; i--) {
-            var child = this._tabitems[i];
-            if (L.DomUtil.hasClass(child, 'active'))
-                L.DomUtil.removeClass(child, 'active');
-        }
+        L.DomUtil.removeClass(this._getActiveTab(), 'active');
+
 
         // close sidebar
         if (!L.DomUtil.hasClass(this._sidebar, 'collapsed')) {
@@ -135,6 +143,34 @@ L.Control.Sidebar = L.Control.extend({
         }
 
         return this;
+    },
+    
+    getActiveTab: function() {
+        return this._getActiveTab();
+    },
+    
+    getDisabledTabs: function() {
+      return this._getDisabledTabs();  
+    },
+    
+    _getActiveTab: function() {
+        for (var i = this._tabitems.length - 1; i >= 0; i--) {
+            var child = this._tabitems[i];
+            if (L.DomUtil.hasClass(child, 'active'))
+               return child;
+        }
+    },
+    
+    _getDisabledTabs: function() {
+        var tabs=[];
+        for (var i = this._tabitems.length - 1; i >= 0; i--) {
+            var tab = this._tabitems[i],
+                child = tab.firstChild;
+            if (L.DomUtil.hasClass(child, 'disabled')){
+               tabs.push(tab);
+            }
+        }  
+        return tabs; 
     },
 
     _onClick: function(e) {
