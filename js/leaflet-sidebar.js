c4a20a8fe5b222d4e5f7829ba89c5782e6ff9f37
nathanvda <nathan@dixis.com>
2016-06-09 22:03:29 +0200

    Add missing closing bracket

--- js/leaflet-sidebar.js
+++ js/leaflet-sidebar.js
@@ -174,7 +174,7 @@ L.Control.Sidebar = L.Control.extend(/** @lends L.Control.Sidebar.prototype */ {
             e.stopPropagation();
             if (L.DomUtil.hasClass(this, 'active')) {
                 this._sidebar.close();
-            } else if (!L.DomUtil.hasClass(this, 'disabled') {
+            } else if (!L.DomUtil.hasClass(this, 'disabled')) {
                 this._sidebar.open(this.querySelector('a').hash.slice(1));
             }
         }
