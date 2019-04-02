5bbb9adaea25b649e9e176d328bbe4508c3a8152
Severin Strobl <github@severin-strobl.de>
2019-04-02 13:28:41 +0200

    Updated to gulp 4.
    The dev dependencies contained multiple packages with known security
    vulnerabilities reported by 'npm audit'. The respective issues are fixed in
    version 4.0.0 of gulp, which required some changes in the configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var csslint = require('gulp-csslint');
 var jshint = require('gulp-jshint');
-var minifyCSS = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
@@ -12,22 +12,21 @@ var pkg = require('./package.json');
 var basename = pkg.name + '-' + pkg.version;
 
 // SASS compilation
-gulp.task('sass', function () {
-    gulp.src('scss/*sidebar.scss')
-        .pipe(sass().on('error', sass.logError))
+gulp.task('sass', function() {
+    return gulp.src('scss/*sidebar.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
         .pipe(gulp.dest('css'));
 });
 
 // Lint JS + CSS
-gulp.task('lint', ['lint:js', 'lint:css']);
-
 gulp.task('lint:js', function() {
   return gulp.src('js/*sidebar.js')
     .pipe(jshint())
     .pipe(jshint.reporter());
 });
 
-gulp.task('lint:css', ['sass'], function() {
+gulp.task('lint:css', gulp.series('sass', function() {
   return gulp.src('css/*sidebar.css')
     .pipe(csslint({
       'adjoining-classes': false,
@@ -38,11 +37,11 @@ gulp.task('lint:css', ['sass'], function() {
       'regex-selectors': false,
     }))
     .pipe(csslint.formatter());
-});
+}));
+
+gulp.task('lint', gulp.parallel('lint:js', 'lint:css'));
 
 // Minify JS + CSS
-gulp.task('minify', ['minify:js', 'minify:css']);
-
 gulp.task('minify:js', function() {
   return gulp.src('js/*sidebar.js')
     .pipe(rename({ suffix: '.min' }))
@@ -50,15 +49,17 @@ gulp.task('minify:js', function() {
     .pipe(gulp.dest('js'));
 });
 
-gulp.task('minify:css', ['sass'], function() {
+gulp.task('minify:css', gulp.series('sass', function() {
   return gulp.src('css/*sidebar.css')
     .pipe(rename({ suffix: '.min' }))
-    .pipe(minifyCSS())
+    .pipe(cleanCSS())
     .pipe(gulp.dest('css'));
-});
+}));
+
+gulp.task('minify', gulp.parallel('minify:js', 'minify:css'));
 
 // Package for distribution
-gulp.task('zip', ['minify'], function() {
+gulp.task('zip', gulp.series('minify', function() {
   return gulp.src([
     'README.md',
     'LICENSE',
@@ -70,13 +71,13 @@ gulp.task('zip', ['minify'], function() {
   }))
   .pipe(zip(basename + '.zip'))
   .pipe(gulp.dest('dist'));
-});
+}));
 
 // Watch JS + CSS Files
-gulp.task('watch', ['lint', 'minify'], function(){
+gulp.task('watch', gulp.series('lint', 'minify', function() {
   gulp.watch('js/*.js', ['lint:js', 'minify:js']);
   gulp.watch('scss/*.scss', ['lint:css', 'minify:css']);
-});
+}));
 
 // Default
-gulp.task('default', ['lint', 'minify']);
+gulp.task('default', gulp.series('lint', 'minify'));
