ed4a9388f2ee1d328293c6e5bde334ea565601bd
Severin Strobl <github@severin-strobl.de>
2019-04-30 10:36:11 +0200

    Integrated support for OL5 based on ES modules.
    OpenLayers 5 is implemented as a set of ES modules, requiring
    major changes to the sidebar. Depending on how OL5 is used, the
    sidebar will either be used as an ES module as well, or, in case the
    full build of OL (ol.js) is used, a version compiled via webpack for
    direct usage in a browser environment is required.

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,35 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var csslint = require('gulp-csslint');
 var jshint = require('gulp-jshint');
-var minifyCSS = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var webpack = require('webpack-stream');
+var named = require('vinyl-named');
+var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
+var lazypipe = require('lazypipe');
 var zip = require('gulp-zip');
 
 var pkg = require('./package.json');
 var basename = pkg.name + '-' + pkg.version;
 
 // SASS compilation
-gulp.task('sass', function () {
-    gulp.src('scss/*sidebar.scss')
-        .pipe(sass().on('error', sass.logError))
+gulp.task('sass', function() {
+    return gulp.src('scss/*sidebar.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
         .pipe(gulp.dest('css'));
 });
 
 // Lint JS + CSS
-gulp.task('lint', ['lint:js', 'lint:css']);
-
 gulp.task('lint:js', function() {
   return gulp.src('js/*sidebar.js')
     .pipe(jshint())
     .pipe(jshint.reporter());
 });
 
-gulp.task('lint:css', ['sass'], function() {
+gulp.task('lint:css', gulp.series('sass', function() {
   return gulp.src('css/*sidebar.css')
     .pipe(csslint({
       'adjoining-classes': false,
@@ -38,27 +41,54 @@ gulp.task('lint:css', ['sass'], function() {
       'regex-selectors': false,
     }))
     .pipe(csslint.formatter());
-});
+}));
+
+gulp.task('lint', gulp.parallel('lint:js', 'lint:css'));
 
 // Minify JS + CSS
-gulp.task('minify', ['minify:js', 'minify:css']);
-
 gulp.task('minify:js', function() {
+  var isESModule = function(file) {
+    var exportRegexp = new RegExp('\\s?export\\s+');
+    var input = file.contents.toString().split('\n');
+    for (i in input) {
+      if (input[i].match(exportRegexp)) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  var jsChannel = lazypipe()
+    .pipe(rename, { suffix: '.min' })
+    .pipe(uglify);
+
+  var esChannel = lazypipe()
+    .pipe(named)
+    .pipe(webpack, {
+      mode: 'production',
+      stats: 'errors-only',
+      output: {
+        filename: '[name].min.js'
+      }
+    });
+
   return gulp.src('js/*sidebar.js')
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
+    .pipe(gulpif(isESModule, esChannel(), jsChannel()))
     .pipe(gulp.dest('js'));
 });
 
-gulp.task('minify:css', ['sass'], function() {
+gulp.task('minify:css', gulp.series('sass', function() {
   return gulp.src('css/*sidebar.css')
     .pipe(rename({ suffix: '.min' }))
-    .pipe(minifyCSS())
+    .pipe(cleanCSS({level: 2}))
     .pipe(gulp.dest('css'));
-});
+}));
+
+gulp.task('minify', gulp.parallel('minify:js', 'minify:css'));
 
 // Package for distribution
-gulp.task('zip', ['minify'], function() {
+gulp.task('zip', gulp.series('minify', function() {
   return gulp.src([
     'README.md',
     'LICENSE',
@@ -70,13 +100,13 @@ gulp.task('zip', ['minify'], function() {
   }))
   .pipe(zip(basename + '.zip'))
   .pipe(gulp.dest('dist'));
-});
+}));
 
 // Watch JS + CSS Files
-gulp.task('watch', ['lint', 'minify'], function(){
+gulp.task('watch', gulp.series('lint', 'minify', function() {
   gulp.watch('js/*.js', ['lint:js', 'minify:js']);
   gulp.watch('scss/*.scss', ['lint:css', 'minify:css']);
-});
+}));
 
 // Default
-gulp.task('default', ['lint', 'minify']);
+gulp.task('default', gulp.series('lint', 'minify'));
